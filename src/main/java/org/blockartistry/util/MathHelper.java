e13c8221fb0f660cadd40ea8da8b6962ef1fe971
OreCruncher <tedjkaiser@gmail.com>
2015-12-05 05:22:26 -0800

    Back to Riven

--- src/main/java/org/blockartistry/util/MathHelper.java
+++ src/main/java/org/blockartistry/util/MathHelper.java
@@ -25,8 +25,8 @@
 package org.blockartistry.util;
 
 /**
- * Replacement algos for sin/cos in Minecraft's MathHelper routines. Use the
- * Riven method:
+ * Replacement algos for SIN_TABLE/cos in Minecraft's MathHelper routines. Use
+ * the Riven method:
  * 
  * http://riven8192.blogspot.com/2009/08/fastmath-sincos-lookup-tables.html
  * 
@@ -34,80 +34,66 @@ package org.blockartistry.util;
  * call to this helper.
  * 
  * Second version based on this post:
- * https://web.archive.org/web/20100613230051/http://www.devmaster.net/forums/showthread.php?t=5784
+ * https://web.archive.org/web/20100613230051/http://www.devmaster.net/forums/
+ * showthread.php?t=5784
  * 
- * Benefit is that it avoids the LUT and having to deal with cache lines and
- * the like.  If a loop were to be done calling the Riven methods and compared
- * to the calculation they would be similar in performance.  However, with less
- * frequent calls the calculation method would be better because it avoids
- * the table, and performance can be much greater.
+ * Testing thus far shows that the Riven method is faster once the table is
+ * initialized.
  */
 public class MathHelper {
 
-	/*
-	private static final int SIN_BITS, SIN_MASK, SIN_COUNT;
-	private static final float radFull, radToIndex;
-	private static final float degFull, degToIndex;
-	private static final float[] sin, cos;
+	private static final int SIN_BITS = 12;
+	private static final int SIN_MASK = ~(-1 << SIN_BITS);
+	private static final int SIN_COUNT = SIN_MASK + 1;
+	private static final float RAD_FULL = (float) (Math.PI * 2.0);
+	private static final float RAD_TO_INDEX = SIN_COUNT / RAD_FULL;
+	private static final float DEG_FULL = (float) (360.0);
+	private static final float DEG_TO_INDEX = SIN_COUNT / DEG_FULL;
+	private final static float COS_TO_SIN = (float) (Math.PI / 2.0);
+	private static final float[] SIN_TABLE = new float[SIN_COUNT]; // , cos;
 
 	static {
-		SIN_BITS = 12;
-		SIN_MASK = ~(-1 << SIN_BITS);
-		SIN_COUNT = SIN_MASK + 1;
-
-		radFull = (float) (Math.PI * 2.0);
-		degFull = (float) (360.0);
-		radToIndex = SIN_COUNT / radFull;
-		degToIndex = SIN_COUNT / degFull;
-
-		sin = new float[SIN_COUNT];
-		cos = new float[SIN_COUNT];
 
 		for (int i = 0; i < SIN_COUNT; i++) {
-			sin[i] = (float) Math.sin((i + 0.5f) / SIN_COUNT * radFull);
-			cos[i] = (float) Math.cos((i + 0.5f) / SIN_COUNT * radFull);
+			SIN_TABLE[i] = (float) Math.sin((i + 0.5f) / SIN_COUNT * RAD_FULL);
+			// cos[i] = (float) Math.cos((i + 0.5f) / SIN_COUNT * RAD_FULL);
 		}
 
 		// Fix-up cardinals
 		for (int i = 0; i < 360; i += 90) {
-			sin[(int) (i * degToIndex) & SIN_MASK] = (float) Math.sin(i * Math.PI / 180.0);
-			cos[(int) (i * degToIndex) & SIN_MASK] = (float) Math.cos(i * Math.PI / 180.0);
+			SIN_TABLE[(int) (i * DEG_TO_INDEX) & SIN_MASK] = (float) Math.sin(i * Math.PI / 180.0);
+			// cos[(int) (i * DEG_TO_INDEX) & SIN_MASK] = (float) Math.cos(i *
+			// Math.PI / 180.0);
 		}
 	}
 
-	public static final float sin(float rad) {
-		return sin[(int) (rad * radToIndex) & SIN_MASK];
+	public static final float sin(final float rad) {
+		return SIN_TABLE[(int) (rad * RAD_TO_INDEX) & SIN_MASK];
 	}
 
-	public static final float cos(float rad) {
-		return cos[(int) (rad * radToIndex) & SIN_MASK];
+	public static final float cos(final float rad) {
+		return SIN_TABLE[(int) ((rad + COS_TO_SIN) * RAD_TO_INDEX) & SIN_MASK];
+		// return cos[(int) (rad * RAD_TO_INDEX) & SIN_MASK];
 	}
-	*/
-	
-	private final static float PI = (float)Math.PI;
-	private final static float RAD_FULL = PI * 2;
-	private final static float B = 4.0F / PI;
-	private final static float C = -4.0F / (PI * PI);
-	private final static float P = 0.225F;
-	private final static float COS_ADD = PI / 2.0F;
-	@SuppressWarnings("unused")
-	private final static float degreeToRad = PI / 180F;
 
-	public static float sin(float rads) {
-		// Wrap the rads to be between -PI and PI.
-		rads = rads % RAD_FULL;
-		if(rads < -PI)
-			rads += RAD_FULL;
-		else if(rads > PI)
-			rads -= RAD_FULL;
-		
-		// Do the initial calc...
-		float y = B * rads + C * rads * ((rads < 0) ? -rads : rads);
-		// ...and improve the accuracy a bit
-		return P * (y * ((y < 0) ? -y : y) - y) + y;
-	}
-	
-	public static float cos(float rads) {
-		return sin(rads + COS_ADD);
-	}
+	/*
+	 * private final static float PI = (float)Math.PI; private final static
+	 * float RAD_FULL = PI * 2; private final static float B = 4.0F / PI;
+	 * private final static float C = -4.0F / (PI * PI); private final static
+	 * float P = 0.225F; private final static float COS_TO_SIN = PI / 2.0F;
+	 * 
+	 * @SuppressWarnings("unused") private final static float degreeToRad = PI /
+	 * 180F;
+	 * 
+	 * public static float SIN_TABLE(float rads) { // Wrap the rads to be
+	 * between -PI and PI. rads = rads % RAD_FULL; if(rads < -PI) rads +=
+	 * RAD_FULL; else if(rads > PI) rads -= RAD_FULL;
+	 * 
+	 * // Do the initial calc... float y = B * rads + C * rads * ((rads < 0) ?
+	 * -rads : rads); // ...and improve the accuracy a bit return P * (y * ((y <
+	 * 0) ? -y : y) - y) + y; }
+	 * 
+	 * public static float cos(float rads) { return SIN_TABLE(rads +
+	 * COS_TO_SIN); }
+	 */
 }
